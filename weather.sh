5cd445e9a45f335fcc8719680ee0f6b807770e91
Dumitru Tronciu <dumitru.tronciu@gmail.com>
2023-06-28 01:07:19 +0300

    Create app script

---
+++ weather.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+apiKey=9cf67574d5224a3bb76120702231606
+locCoord="46.06667,23.58333"
+curl "http://api.weatherapi.com/v1/current.json?key=$apiKey&q=$locCoord" > w.json
+
+wtrStatus=$( cat w.json | jq -r '.current.condition.text' )
+goodwtr=😀
+badwtr=🙁
+#echo "weather status: $wtrStatus"
+#test -z $wtrStatus  && echo "Something went wrong"
+if [ "$wtrStatus" == "Sunny" ]; then
+	echo $goodwtr
+else
+	echo $badwtr
+fi
+
+rm w.json
