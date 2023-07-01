caf9f4b633c6105f52a431958eebc5ca5ee5f769
Dumitru Tronciu <dumitru.tronciu@gmail.com>
2023-07-01 18:56:28 +0300

    feat: added implementation for emojis

--- weather.sh
+++ weather.sh
@@ -6,21 +6,43 @@ curl "http://api.weatherapi.com/v1/current.json?key=$apiKey&q=$locCoord" > w.jso
 
 wtrStatus=$( cat w.json | jq -r '.current.condition.text' )
 declare -A emoji=(
-	[happy]=😀,
-	[sad]=🙁,
-	[sunny]=🌞,
-	[partly_cloudy]=⛅,
-	[sunshower]=🌦,
-	[rain]=🌧,
-	[snow]=🌨,
+	[happy]=😀
+	[sad]=🙁
+	[sunny]=🌞
+	[partly_cloudy]=⛅
+	[sunshower]=🌦
+	[rain]=🌧
+	[snow]=🌨
 	[thunderstorm]=🌩
+	[fog]=🌫
 )
 #echo "weather status: $wtrStatus"
 #test -z $wtrStatus  && echo "Something went wrong"
-if [ "$wtrStatus" == "Sunny" ]; then
-	echo $goodwtr
-else
-	echo $badwtr
-fi
+case $wtrStatus in
+	"Sunny")
+		echo ${emoji[sunny]}
+		;;
+	"Partly cloudy")
+		echo ${emoji[partly_cloudy]}
+		;;
+	"Patchy light rain" | "Patchy light drizzle" | "Light drizzle")
+		echo ${emoji[sunshower]}
+		;;
+	"Light rain" | "Moderate rain at times" | "Moderate rain")
+		echo ${emoji[rain]}
+		;;
+	"Moderate or heavy rain with thunder" | "Patchy light rain with thunder")
+		echo ${emoji[thunderstorm]}
+		;;
+	"Light snow" | "Patchy light snow" | "Patchy moderate snow" | "Moderate snow" \
+	| "Patchy heavy snow" | "Patchy heavy snow" | "Heavy snow")
+		echo ${emoji[snow]}
+		;;
+	"Fog")
+		echo ${emoji[snow]}
+		;;
+	*)
+		echo ${emoji[sad]}
+esac
 
 rm w.json
