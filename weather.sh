85673526ccc1b70d06f7580c3c1454b159aaa8ae
Bosna28 <bosna28vk@mail.ru>
2023-07-03 14:27:35 +0300

    Print-Line Api_Key(doesn't inserted) and bosna28-Ubuntu-bosna28

--- weather.sh
+++ weather.sh
@@ -2,6 +2,7 @@
 
 if [[ -z "${API_KEY}" ]]; then
 	wtrEmoji="API_KEY isn't sett"
+ 	echo "Api_Key is not set";
 	exit -1
 fi
 
@@ -53,7 +54,7 @@ case $wtrStatus in
 		wtrEmoji=${emoji[sad]}
 esac
 
-echo "Hello ${HOSTNAME},"
+echo "Hello ${USER},"
 echo "Current weather in ${city}: ${wtrStatus} ${wtrEmoji}"
 
 
